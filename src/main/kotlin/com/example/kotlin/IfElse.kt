9a108a526f2dda73d7f9dfcf80dbaa593dc3c035
sh-doveloper <sh.doveloper@gmail.com>
2024-02-16 11:56:01 +0900

    feat: 코틀린 최초 셋팅 및 문법 variable, ifelse, fun 추가

---
+++ src/main/kotlin/com/example/kotlin/IfElse.kt
@@ -0,0 +1,28 @@
+package com.example.kotlin
+
+fun main(){
+
+    // if else 사용
+    val job = "Software Developer"
+    if (job == "Software Developer"){
+        println("개발자")
+    }else{
+        println("개발자아님")
+    }
+
+    // 코틀린의 if else 는 표현식
+    val age :Int = 10
+    val str = if(age > 10){
+        "성인"
+    }else{
+        "아이"
+    }
+    println(str)
+
+    // 코틀린은 삼항연산자 없다. if else 표현식으로 불필요
+    val a = 1
+    val b = 2
+    val c = if(b > a) b else a
+    println(c)
+
+}
